0f123537f538a8102552200b628f4cf4b76fdbe1
Jedi30 <czarchris.mariano@gmail.com>
2023-10-18 10:28:31 +0800

    added exercise boarders

--- main.js
+++ main.js
@@ -3,14 +3,14 @@
 //if hour is between 12pm and 6pm : Good afternoon!
 //otherwise: Good Evening!
 
-let hour = 19;
+boarders = 15;
 
-if(hour >= 6 && hour <12)
-    console.log("Good Morning!");
-
-else if (hour >=12 && hour < 18)
-    console.log("Good Afternoon");
-
-else 
-    console.log("Good Evening");
-
+if(boarders >=10 && boarders < 15){
+    console.log("Hindi ka Kumita");
+}
+else if (boarders>=15 && boarders< 20){
+    console.log("HKumita Ka!");
+}
+else{
+    console.log("Lugi!");
+}
