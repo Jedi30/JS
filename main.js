3b9dce04db68cf4cbc6cf45ab2047718745ac6f7
Jedi30 <czarchris.mariano@gmail.com>
2023-10-18 10:12:49 +0800

    added else if

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-//OBJect
+//Hour
+//if hour is between 6am and 12pm : Good morning!
+//if hour is between 12pm and 6pm : Good afternoon!
+//otherwise: Good Evening!
 
-class Tao {
-    constructor(firstName, lastName,bt){
-        this.firstName;
-        this.lastName;
-        this.bt;
-    }
-    
+let hour = 13;
+
+if(hour >= 6 && hour <12){
+    console.log("Good Morning!");
+}
+else if (hour >=12 && hour < 18){
+    console.log("Good Afternoon");
+}
+else {
+    console.log("Good Evening");
 }
