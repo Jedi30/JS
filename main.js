2183eeb331359fae2767ce7849511c8e72e8ff61
Jedi30 <czarchris.mariano@gmail.com>
2023-10-23 09:22:44 +0800

    added switch statement

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
-//Not operator
+//switch
 
-alert( !0);
+witch(favoritefruit){
+
+    case"mango":
+        console.log("Very delicious fruit")
+        break
+
+    case"Apple":
+    console.log("Delicious too")
+    break
+
+    case "Durian":
+    console.log("Exotic Food")
+    break
+
+    default
+    console.log("Never tasted this fruit before")
+
+
+    }
+}
 
 
