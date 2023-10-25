0dd940a3a5d059936d12705c600c06730fecc4eb
Jedi30 <czarchris.mariano@gmail.com>
2023-10-25 13:58:46 +0800

    added Hello World

--- main.js
+++ main.js
@@ -1,23 +1,4 @@
-//switch
-  const favoritefruit="mango"
-
-switch(favoritefruit){
-
-    case "mango":
-        console.log("My most favorite fruit")
-        break;
-
-    case "banana":
-            console.log("Rich in Potassium")
-            break;
-
-    case "Watermelon":
-         console.log("refreshing fruit")
-        break;
-
-
-    default:
-                console.log("I love fruitsalad")
-                break;
-}
+  const myheading= document.queryselector("h1");
+  myheading.textContent= "Hello World!";
+   
 
